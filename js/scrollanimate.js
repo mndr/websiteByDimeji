50fca086b6eb192e1d55bac2d214376f5566f086
mndr <amandolin@ymail.com>
2017-03-24 20:49:07 -0500

    whatever

---
+++ js/scrollanimate.js
@@ -0,0 +1,19 @@
+"use strict";
+
+$(document).ready(function(){
+    $(".navbar li a").click(function(){
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $hash = this.hash;
+            var $target = $(this.hash);
+            if ($target.length) {
+                var targetOffset = $target.offset().top - 90;
+                $('html,body').animate({scrollTop: targetOffset}, 1000, function() {
+                    window.location.hash = $hash;
+
+                });
+                return false;
+            }
+        }
+    });
+});
